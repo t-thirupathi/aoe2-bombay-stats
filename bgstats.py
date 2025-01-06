084ac33402c58147a984727d6d8e73094d9872a1
Thirupathi Thangavel <thirupathi.thangavel@gmail.com>
2025-01-06 11:44:44 +0530

    bg stats

---
+++ bgstats.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import json
+
+with open('BGStatsExport-241227060814.json', 'r') as file:
+    bg_stats = file.read()
+
+data = json.loads(bg_stats)
+# df = pd.DataFrame(data)
+# print(data['plays'])
+plays_df = pd.DataFrame(data['plays'])
+print(plays_df.columns)
